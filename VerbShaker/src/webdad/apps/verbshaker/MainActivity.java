b691de88ba85a74070457655402b0720b9482805
Dsigmund <sigmund.dominik@googlemail.com>
2013-02-20 14:47:56 +0100

    Added Shake functionality

--- VerbShaker/src/webdad/apps/verbshaker/MainActivity.java
+++ VerbShaker/src/webdad/apps/verbshaker/MainActivity.java
@@ -2,21 +2,36 @@ package webdad.apps.verbshaker;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener{
 
 	public DataBaseHelper db;
 	public TextView txt_m;
 	private ProgressDialog pd;
 	
+	private SensorManager mSensorManager;
+	private Sensor mSensor;
+	private int sensor_changes;
+	private static final int SHAKE_THRESHOLD = 800;
+	private long lastUpdate=0;
+	float x, y, z, last_x=0.0f, last_y=0.0f, last_z = 0.0f, gravity_x=0.0f, gravity_y=0.0f, gravity_z=0.0f;
+	final float alpha = (float) 0.8;
+	
+	private Vibrator vib;
+
 	//IN
 	
 	@Override
@@ -28,6 +43,12 @@ public class MainActivity extends Activity {
 		Log.i("App", "Create DB if needed...");
 		db.CreateMe();
 		txt_m = (TextView)findViewById(R.id.txt_mixed);
+		mSensorManager = (SensorManager) getSystemService(getApplicationContext().SENSOR_SERVICE);
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+		    mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		  }
+		sensor_changes=0;
+		vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 		Log.i("App", "Ready!");
 	}
 	
@@ -43,6 +64,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onResume(){
 		super.onResume();
+		 mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
 	}
 	
 	//resumed (visible)
@@ -50,6 +72,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onPause(){
 		super.onPause();
+		mSensorManager.unregisterListener(this);
 	}
 	
 	//paused (maybe visible)
@@ -144,4 +167,45 @@ public class MainActivity extends Activity {
             }
         };
     }
+
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Do Something for an Acc change
+		
+	}
+
+	public void onSensorChanged(SensorEvent event) {
+		Log.i("Sensor",event.toString());
+
+		long curTime = System.currentTimeMillis();
+	    // only allow one update every 100ms.
+	    if ((curTime - lastUpdate) > 100) {
+	      long diffTime = (curTime - lastUpdate);
+	      lastUpdate = curTime;
+
+	      
+
+	      // Isolate the force of gravity with the low-pass filter.
+	      gravity_x = alpha * gravity_x + (1 - alpha) * event.values[0];
+	      gravity_y = alpha * gravity_y + (1 - alpha) * event.values[1];
+	      gravity_z = alpha * gravity_z + (1 - alpha) * event.values[2];
+
+	      // Remove the gravity contribution with the high-pass filter.
+	      x = event.values[0] - gravity_x;
+	      y = event.values[1] - gravity_y;
+	      z = event.values[2] - gravity_z;
+
+
+	      float speed = Math.abs(x+y+z - (last_x + last_y + last_z)) / diffTime * 10000;
+
+	      if (speed > SHAKE_THRESHOLD) {
+	        Log.d("sensor", "shake detected w/ speed: " + speed);
+	        getMix();
+	        vib.vibrate(300);
+	      }
+	      last_x = x;
+	      last_y = y;
+	      last_z = z;
+	    }
+		
+	}
 }
