8e0a9fa55087e511f99284374251963521cc007a
Dsigmund <sigmund.dominik@googlemail.com>
2013-02-20 14:17:17 +0100

    Made Sync Work.

---
+++ VerbShaker/src/webdad/apps/verbshaker/DataBaseHelper.java
@@ -0,0 +1,152 @@
+package webdad.apps.verbshaker;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+	private static final int DATABASE_VERSION = 1;
+    private static final String DICTIONARY_TABLE_NAME = "verbs_de";
+	private static final String DATABASE_NAME = "proverbs";
+
+	private Context myContext;
+
+    DataBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        myContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    	Log.i("DB","Database is going to be created.");
+        db.execSQL("CREATE TABLE "+DICTIONARY_TABLE_NAME+" (id INT auto_increment, front TEXT, back TEXT)"); 
+        initialInsert(db);
+    }
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		if(arg2<arg1)return;
+		if(arg0.isReadOnly())return;
+		//TODO upgrade when we wrote an update and chaned DBVERSION(Arg2)
+	}
+	
+	public void CreateMe(){
+		try {
+		SQLiteDatabase d = this.getWritableDatabase();
+		d.close();
+		}
+		catch(Exception e){
+			Log.e("DB",e.getMessage());
+		}
+	}
+	
+	public void initialInsert(SQLiteDatabase db){
+		Log.i("DB","Inserting initial Content");
+		AssetManager mngr = myContext.getAssets();
+	    try {
+			InputStream is = mngr.open(DICTIONARY_TABLE_NAME+".csv");
+			// foreach line split it at | and then insert it
+			InputStreamReader inputStreamReader = new InputStreamReader(is);
+		    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		    String line;
+		    while ((line = bufferedReader.readLine()) != null) {
+		        String[] tmp = line.split("\\|");
+		        db.execSQL("INSERT INTO "+DICTIONARY_TABLE_NAME+" (front, back) VALUES ('"+tmp[0]+"', '"+tmp[1]+"')"); 
+		        Log.i("DB","INSERT INTO "+DICTIONARY_TABLE_NAME+" (front, back) VALUES ('"+tmp[0]+"', '"+tmp[1]+"')");
+		    }
+		    bufferedReader.close();
+		    inputStreamReader.close();
+		    is.close();
+		    mngr.close();
+		} catch (IOException e) {
+			Log.e("DB","Could not open "+DICTIONARY_TABLE_NAME+".csv");
+		}			
+	}
+	
+	public Boolean Sync(){
+		Boolean ok = true;
+		SQLiteDatabase d = this.getWritableDatabase();
+
+		
+
+		   Log.i("Sync", "Getting HTTP");
+		   HttpClient client = new DefaultHttpClient();
+		   HttpGet request = new HttpGet("http://verbshaker.webdad.eu/sync.php");
+		   HttpResponse response = null;
+			try {
+				response = client.execute(request);
+			} catch (ClientProtocolException e) {
+				Log.e("Sync","CPE "+e.getMessage());
+			} catch (IOException e) {
+				Log.e("Sync","IOE "+e.getMessage());
+			}
+		   InputStream in = null;
+			try {
+				in = response.getEntity().getContent();
+			} catch (IllegalStateException e) {
+				Log.e("Sync","ISE "+e.getMessage());
+			} catch (IOException e) {
+				Log.e("Sync","IOE "+e.getMessage());
+			}
+			InputStreamReader inputStreamReader = new InputStreamReader(in);
+		    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		    String line;
+		    Log.i("Sync", "Truncating "+DICTIONARY_TABLE_NAME);
+			d.execSQL("DELETE FROM "+DICTIONARY_TABLE_NAME);
+		    try{
+		    while ((line = bufferedReader.readLine()) != null) {
+		        String[] tmp = line.split("\\|");
+		        d.execSQL("INSERT INTO "+DICTIONARY_TABLE_NAME+" (front, back) VALUES ('"+tmp[0]+"', '"+tmp[1]+"')"); 
+		        Log.i("Sync","INSERT INTO "+DICTIONARY_TABLE_NAME+" (front, back) VALUES ('"+tmp[0]+"', '"+tmp[1]+"')");
+		    }
+		    bufferedReader.close();
+		    inputStreamReader.close();
+		    in.close();
+		    }
+		    catch(Exception e){
+		    	Log.e("Sync",e.getMessage());
+		    }
+		d.close();
+		Log.i("Sync", "Sync Complete");
+		return ok;
+	}
+	
+	public String getProVerb(){
+		String r = "";
+		try{
+		SQLiteDatabase d = this.getWritableDatabase();
+		Cursor rf = d.rawQuery("SELECT front FROM "+DICTIONARY_TABLE_NAME+" ORDER BY RANDOM() ASC LIMIT 1", null);
+		Cursor rb = d.rawQuery("SELECT back FROM "+DICTIONARY_TABLE_NAME+" ORDER BY RANDOM() ASC LIMIT 1", null);
+		
+		rf.moveToFirst();
+		rb.moveToFirst();
+		
+		r += rf.getString(0) + " "+ rb.getString(0);
+		
+		rf.close();
+		rb.close();
+		d.close();
+		}
+		catch(Exception e){
+			Log.e("DB", e.getMessage());
+			r = "An Error has occured...";
+		}
+		return r;
+	}
+
+}
--- VerbShaker/src/webdad/apps/verbshaker/MainActivity.java
+++ VerbShaker/src/webdad/apps/verbshaker/MainActivity.java
@@ -1,8 +1,13 @@
 package webdad.apps.verbshaker;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,21 +15,101 @@ public class MainActivity extends Activity {
 
 	public DataBaseHelper db;
 	public TextView txt_m;
+	private ProgressDialog pd;
+	
+	//IN
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Log.i("App", "Starting...");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		db = new DataBaseHelper(getApplicationContext());
+		Log.i("App", "Create DB if needed...");
 		db.CreateMe();
 		txt_m = (TextView)findViewById(R.id.txt_mixed);
+		Log.i("App", "Ready!");
 	}
-
+	
+	//created
+	
+	@Override
+	protected void onStart(){
+		super.onStart();
+	}
+	
+	//started(visible)
+	
+	@Override
+	protected void onResume(){
+		super.onResume();
+	}
+	
+	//resumed (visible)
+	
+	@Override
+	protected void onPause(){
+		super.onPause();
+	}
+	
+	//paused (maybe visible)
+	
+	@Override
+	protected void onStop(){
+		super.onStop();
+	}
+	
+	//stopped
+	
+	@Override
+	protected void onDestroy(){
+		db.close();
+		super.onDestroy();
+	}
+	
+	//OUT
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		try{
+			getMenuInflater().inflate(R.menu.activity_main, menu);
+		}
+		catch(Exception e){
+			Log.e("App",e.getMessage());
+		}
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.menu_settings:
+	        	/*try
+	        	{
+	        	Intent in = new Intent(MainActivity.this, preferences.class);
+	        	MainActivity.this.startActivity(in);
+	        	}
+	        	catch(Exception e)
+	        	{
+	        	Log.e("Menu", e.getMessage());
+	        	}*/
+	        	
+	            return true;
+	        case R.id.menu_sync:
+	        	try
+	        	{
+	        		sync();
+	        	}
+	        	catch(Exception e)
+	        	{
+	        		Log.e("Menu", e.getMessage());
+	        	}
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 	
 	public void btn_getNewMix_onclick(View view) {
@@ -33,6 +118,30 @@ public class MainActivity extends Activity {
 	
 	public void getMix(){
 		txt_m.setText(db.getProVerb());
-		// TODO: get a new mix from db and fill txt_mixed with is
 	}
+	
+	private void sync(){
+		pd = ProgressDialog.show(MainActivity.this, "", "Syncing with Online DB", true, false);
+		SyncThread st = new SyncThread();
+	    st.start();
+	}
+	
+	private class SyncThread extends Thread {
+        public SyncThread() {
+        }
+
+        @Override
+        public void run() {         
+            db.Sync();
+            handler.sendEmptyMessage(0);
+        }
+
+		private Handler handler = new Handler() {
+
+            @Override
+            public void handleMessage(Message msg) {
+                pd.dismiss();
+            }
+        };
+    }
 }
