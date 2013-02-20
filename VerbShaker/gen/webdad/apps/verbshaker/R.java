c4c4a1b0d451d55f7c5e6d1516dcd22428af5bdf
Dsigmund <sigmund.dominik@googlemail.com>
2013-02-20 10:55:39 +0100

    Added Database and Basic GUI

--- VerbShaker/gen/webdad/apps/verbshaker/R.java
+++ VerbShaker/gen/webdad/apps/verbshaker/R.java
@@ -14,7 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int btn_getNewMix=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int menu_sync=0x7f070003;
+        public static final int txt_mixed=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -24,8 +27,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int btn_getMix_Text=0x7f040003;
+        public static final int menu_settings=0x7f040001;
+        public static final int menu_sync=0x7f040004;
+        public static final int starter=0x7f040002;
     }
     public static final class style {
         /** 
--- VerbShaker/src/webdad/apps/verbshaker/MainActivity.java
+++ VerbShaker/src/webdad/apps/verbshaker/MainActivity.java
@@ -1,15 +1,23 @@
 package webdad.apps.verbshaker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	public DataBaseHelper db;
+	public TextView txt_m;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		db = new DataBaseHelper(getApplicationContext());
+		db.CreateMe();
+		txt_m = (TextView)findViewById(R.id.txt_mixed);
 	}
 
 	@Override
@@ -18,5 +26,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
+	
+	public void btn_getNewMix_onclick(View view) {
+	     getMix();
+	 }
+	
+	public void getMix(){
+		txt_m.setText(db.getProVerb());
+		// TODO: get a new mix from db and fill txt_mixed with is
+	}
 }
