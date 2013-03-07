a67b4e2ce9268575d1a4601f281fbd2f261e195c
Dsigmund <sigmund.dominik@googlemail.com>
2013-03-07 14:24:19 +0100

    Replaced Logo with Logo by Janine Denk

--- VerbShaker/src/webdad/apps/verbshaker/DataBaseHelper.java
+++ VerbShaker/src/webdad/apps/verbshaker/DataBaseHelper.java
@@ -18,7 +18,6 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.provider.ContactsContract.Contacts.Data;
 import android.util.Log;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
